c228982d5f787f53e0be4f8ff324aeda1e76d023
Valerie Don <valeriejdon@gmail.com>
2020-03-06 08:21:03 -0800

    Create a Weather object view model

---
+++ OnRamp-Take-Home-Project/WeatherViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherViewModel.swift
+//  OnRamp-Take-Home-Project
+//
+//  Created by Valerie Don on 3/6/20.
+//  Copyright © 2020 Valerie Don. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewModel {
+
+}

dab3fabb32c3de2452181e97d68ed8dba2b25e7f
Valerie Don <valeriejdon@gmail.com>
2020-03-06 00:23:22 -0800

    Add custom UITableViewCell

---
+++ OnRamp-Take-Home-Project/ForecastTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ForecastTableViewCell.swift
+//  OnRamp-Take-Home-Project
+//
+//  Created by Valerie Don on 3/6/20.
+//  Copyright © 2020 Valerie Don. All rights reserved.
+//
+
+import UIKit
+
+class ForecastTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var forecastLabel: UILabel!
+
+}

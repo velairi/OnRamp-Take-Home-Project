c8faaf4188b971b304128d89b20ec3cbe3798469
Valerie Don <valeriejdon@gmail.com>
2020-03-06 00:13:50 -0800

    Set up tableview methods

--- OnRamp-Take-Home-Project/HomeViewController.swift
+++ OnRamp-Take-Home-Project/HomeViewController.swift
@@ -8,12 +8,26 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableview: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tableview.dataSource = self
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        // TO DO: return the user's current location
+        return "San Francisco"
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
     }
 }
